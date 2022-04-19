58e28a18443ff82086823f621fe581f7e79f478f
vjohndo <v.john.do@outlook.com>
2022-04-20 00:14:48 +1000

    compiling and running java files

--- Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
+++ Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
@@ -1,6 +1,12 @@
 // TASK 1: Make a class.
-
-
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Printing is fun!");
+        System.out.println("Java > Python.");
+        System.out.println("I spilled Java all over my paper");
+        System.out.println("My dog ate my Java.");
+    }
+}
 
 // TASK 2: Inside the class, write the main() method.
 
